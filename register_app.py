4d5477d58406c92f5a79cd0ef4d7d997531a72fb
WGrav <63617338+WGrav01@users.noreply.github.com>
2024-05-26 18:18:23 -0400

    created register_app.py, added secrets file to git

---
+++ register_app.py
@@ -0,0 +1,14 @@
+from mastodon import Mastodon
+
+# from mastodon.py documentation:
+"""
+Register your app!
+ This only needs to be done once (per server, or when distributing rather than hosting an application,
+  most likely per device and server).
+  substitute in your information:
+"""
+
+
+Mastodon.create_app(
+    "cahbot", api_base_url="https://botsin.space", to_file="cahbot_clientcred.secret"
+)
